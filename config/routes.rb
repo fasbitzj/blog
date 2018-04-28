2cf37d556dc0fcf634a2838d9e2f461f78fa609f
fasbitzj <fasbit@163.com>
2018-04-28 17:57:59 +0800

    init

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :people
+
+  get "welcome/say_hello" => "welcome#say"
+  get "welcome" => "welcome#index"
+  # root :to => "welcome#index"
+
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  root 'static_pages#home'
+end
