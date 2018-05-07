91dd78039add9e61d7e5c5b25db6757910ea2631
fasbitzj <fasbit@163.com>
2018-05-07 19:30:11 +0800

    add remember me

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save{ self.email = email.downcase}
 
   validates(:name,
@@ -16,10 +18,33 @@ class User < ApplicationRecord
 
   validates :password, presence: true, length: {minimum: 6}
 
-  # 返回指定字符串的哈希摘要
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-               BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  # remember me
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  class << self
+    # 返回指定字符串的哈希摘要
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                 BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # 返回随机令牌
+    # 用于功能：remember me
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 end
